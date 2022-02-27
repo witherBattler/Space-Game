ad6e3be06c639ed0d425d1e23439fed658725bc8
IMPixel <asemenov.ca@gmail.com>
2022-02-27 12:51:26 -0500

    menu

--- drawingUI.js
+++ drawingUI.js
@@ -7,7 +7,7 @@ var spaceshipPowersColors = {
     "Magma": "orangered",
     "Pink Plasma": "hotpink"
 }
-var unlockedSpaceshipPowers = ["Magma"]
+var unlockedSpaceshipPowers = ["Magma", "Pink Plasma"]
 
 var unlockedSpaceshipTypes = ["cargoship", "shuttlenoweps", "destroyer"]
 var allSpaceshipTypes = ["cargoship", "carrier", "cruiser", "destroyer", "shuttlenoweps"]
@@ -22,7 +22,14 @@ function drawMenuUI() {
     var alphaValue = mouseOverRect(145, height / 2, 270, 480) ? 75 : 50
     fill(color(255, 255, 255, alphaValue))
     rectMode(CENTER)
-    rect(145, height / 2, 270, 480, 30, 30, 30, 30)
+    noStroke()
+    rect(145, height / 2, 250, 460, 20, 20, 20, 20)
+    drawSideBarButton("Endless", 32)
+    drawSideBarButton("Levels", 107)
+    drawSideBarButton("Leaderboard", 182)
+    drawSideBarButton("Settings", 257)
+    drawSideBarButton("Tutorial", 332)
+    drawSideBarButton("Credits", 407)
     
     //Title
     fill("white")
@@ -73,6 +80,31 @@ function drawMenuUI() {
     var currentSpaceshipImage = getSpaceshipImage(spaceshipConfiguration.type, spaceshipConfiguration.power)
     image(currentSpaceshipImage, 600, 400, currentSpaceshipImage.width / 2, currentSpaceshipImage.height / 2)
 
+    
+    var stats = getSpaceshipStatistics(spaceshipConfiguration.type, spaceshipConfiguration.power)
+    noStroke()
+    rectMode(CORNER)
+    textAlign(LEFT)
+
+    fill("lightblue")
+    rect(750, 300 + 50, stats.speed / 20 * 400, 20)
+    text("Speed", 760 + stats.speed / 20 * 400, 315 + 50)
+
+    fill("red")
+    rect(750, 320 + 50, stats.bulletDamage / 100 * 400, 20)
+    text("Bullet Damage", 760 + stats.bulletDamage / 100 * 400, 336 + 50)
+
+    fill("lightgreen")
+    rect(750, 340 + 50, stats.bulletSpeed / 30 * 400, 20)
+    text("Bullet Speed", 760 + stats.bulletSpeed / 30 * 400, 356 + 50)
+
+    fill("blue")
+    rect(750, 360 + 50, stats.protectionLevel / 5 * 400, 20)
+    text("Protection", 760 + stats.protectionLevel / 5 * 400, 376 + 50)
+
+    fill("pink")
+    rect(750, 380 + 50, stats.reloadTime / 120 * 400, 20)
+    text("Reload Time", 760 + stats.reloadTime / 120 * 400, 396 + 50)
 }
 
 function drawStarBackground() {
@@ -115,13 +147,23 @@ function uiMousePressed () {
 function uiMouseMoved() {
     switch(stage) {
         case "menu":
-        var mouseIsPointer = mouseOverRect(725 - 200, 150, 30, 40) || mouseOverRect(725 + 200, 150, 30, 40)
+        var mouseIsPointer = 
+        //Arrows
+        mouseOverRect(725 - 200, 150, 30, 40)
+        || mouseOverRect(725 + 200, 150, 30, 40)
         //Checking for spaceship mouseover's
         || mouseOverRect(725 - 180, 250, 100, 50)
         || mouseOverRect(725 - 83, 250, 75, 50)
         || mouseOverRect(725, 250, 75, 40)
         || mouseOverRect(725 + 95, 250, 90, 80)
         || mouseOverRect(725 + 180, 250, 50, 30)
+        //Side bar buttons
+        || mouseOverRect(45, 32, 200, 60, "corner")
+        || mouseOverRect(45, 107, 200, 60, "corner")
+        || mouseOverRect(45, 182, 200, 60, "corner")
+        || mouseOverRect(45, 257, 200, 60, "corner")
+        || mouseOverRect(45, 332, 200, 60, "corner")
+        || mouseOverRect(45, 407, 200, 60, "corner")
         cursor(mouseIsPointer ? "pointer" : "default")
         break;
     }
--- util.js
+++ util.js
@@ -1,10 +1,17 @@
 function mouseOverRect(x, y, width, height, type = "center") {
     switch(type) {
         case "center":
-        if(mouseX > x - width / 2 && mouseX < x + width / 2 && mouseY > y - height / 2 && mouseY < y + height / 2) {
-            return true
-        }
-        break;
+            if(mouseX > x - width / 2 && mouseX < x + width / 2 && mouseY > y - height / 2 && mouseY < y + height / 2) {
+                return true
+            }
+            break;
+        case "corner":
+            if(mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height) {
+                return true
+            }
+            break;
+        default: 
+            throw new Error("Wrong mouseOverRect type property passed")
     }
     return false
 }
@@ -33,4 +40,75 @@ function elementIsLast(element, array) {
 
 function getSpaceshipImage(type, power) {
     return spaceshipImages[power][type]
+}
+
+function getSpaceshipStatistics(type, power) {
+    var speed = power == "Pink Plasma" ? 10 : 0 //20
+    var bulletDamage = power == "Pink Plasma" ? 50 : 0 //100
+    var bulletSpeed = power == "Pink Plasma" ? 15 : 0 //30
+    var protectionLevel = power == "Pink Plasma" ? 1 : 0 //4
+    var reloadTime = 120
+    switch(type) {
+        case "cargoship":
+            speed += 5
+            bulletDamage += 25
+            bulletSpeed += 7.5
+            protectionLevel += 2;
+            reloadTime -= 90
+            break;
+        case "carrier":
+            speed += 5
+            bulletDamage += 5
+            bulletSpeed += 15
+            protectionLevel += 2;
+            reloadTime -= 110
+            break;
+        case "cruiser":
+            speed += 8
+            bulletDamage += 35
+            bulletSpeed += 6
+            protectionLevel += 1.5;
+            reloadTime -= 60
+            break;
+        case "destroyer":
+            speed += 3
+            bulletDamage += 50
+            bulletSpeed += 3
+            protectionLevel += 2;
+            reloadTime -= 60
+            break;
+        case "shuttlenoweps":
+            speed += 10
+            bulletDamage += 15
+            bulletSpeed += 15
+            protectionLevel += 1;
+            reloadTime -= 90
+            break;
+    }
+    return {
+        speed: speed,
+        bulletDamage: bulletDamage,
+        bulletSpeed: bulletSpeed,
+        protectionLevel: protectionLevel,
+        reloadTime: reloadTime
+    }
+}
+
+function drawSideBarButton(textToWrite, y) {
+    //Button properties
+    stroke("black")
+    strokeWeight(5)
+    rectMode(CORNER)
+
+    //Fill color
+    fill(mouseOverRect(45, y, 220, 60, "corner") ? "#960f00" : "#c71400")
+
+    //Drawing Button
+    rect(35, y, 220, 60, 10, 10, 10, 10)
+    noStroke()
+    fill("black")
+    textFont(ethnocentric)
+    textAlign(CENTER)
+    textWidth(textToWrite) < 200 ? textSize(20) : textSize(17)
+    text(textToWrite, 145, y + 37)
 }
